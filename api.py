15a7d91cb293a246ff64e4c82d00fe5c72e3b566
etwither <etw17@fsu.edu>
2023-04-25 08:49:58 -0500

    changed openapi to openai

--- api.py
+++ api.py
@@ -1,3 +1,17 @@
 import openapi
 
 openapi.api_key = "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"
+
+prompt = "Hello, how can I help you today?"
+model = "text-davinci-002" # Or any other model you want to use
+response = openai.Completion.create(
+    engine=model,
+    prompt=prompt,
+    max_tokens=100,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+
+print(response.choices[0].text)
+
