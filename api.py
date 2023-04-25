0a5e9dac3adff5a8cacb5a14489c71979605582a
etwither <etw17@fsu.edu>
2023-04-25 09:51:31 -0500

    api.py is changed to a function

--- api.py
+++ api.py
@@ -1,18 +1,18 @@
 import openai
-openai.api_key = "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"
 
-
-
-prompt = input("please enter a question")
-
-model = "text-davinci-002" # Or any other model you want to use
-response = openai.Completion.create(
-    engine=model,
-    prompt=prompt,
-    max_tokens=100,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
-
-print(response.choices[0].text)
+def aip_func():
+	key = imput("Enter api key: ")
+	openai.api_key = key
+	prompt = input("please enter a question: ")
+	
+	model = "text-davinci-002" # Or any other model you want to use
+	response = openai.Completion.create(
+		engine=model,
+		prompt=prompt,
+		max_tokens=100,
+		n=1,
+		stop=None,
+		temperature=0.5,
+	)
+	
+	print(response.choices[0].text)
